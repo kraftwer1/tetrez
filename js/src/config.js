c03dd889efac0bfe2cd782df443d4337c5c238c4
Alex Kühne <alex@nordformat.ch>
2016-03-13 10:22:25 +0100

    Added config file; Merged engine and 3D representation; Added 3D debug mode

---
+++ js/src/config.js
@@ -0,0 +1,11 @@
+(function() {
+    window.Tetrez = {
+        config: {
+            dimension: {
+                x: 10,
+                y: 18
+            },
+            initSpeed: 500
+        }
+    };
+}());
--- js/src/main.js
+++ js/src/main.js
@@ -1,187 +1,193 @@
+// TODO
+// - Blocks should fall from above the plane
+
 (function() {
-    var Block = function() {
-        this.matrix = [
-            [0, 0],
-            [0, 1],
-            [0, 2],
-            [1, 1]
-        ];
+    // Init field, multidimensional with "1" values
+    var field = [];
+
+    // Hope not!
+    var isGameOver = false;
+
+    var gameInterval = null;
+
+    for (var i = 0; i < Tetrez.config.dimension.y; ++i) {
+        field[i] = [];
+
+        for (var j = 0; j < Tetrez.config.dimension.x; ++j) {
+            field[i][j] = 1;
+        }
+    }
+
+    // Uncomment for field debugging
+    // field = [
+    //     [1, 1, 1, 1],
+    //     [1, 1, 1, 1],
+    //     [1, 1, 1, 1],
+    //     [1, 1, 1, 1],
+    //     [1, 1, 1, 1],
+    //     [1, 1, 1, 1]
+    // ];
+
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var aspectRatio = width / height;
+    var viewSize = Tetrez.config.dimension.y;
+
+    var camera = new THREE.OrthographicCamera(
+        -(aspectRatio * viewSize / 2),
+        (aspectRatio * viewSize / 2),
+        (viewSize / 2),
+        -(viewSize / 2)
+    );
+
+    camera.position.z = viewSize;
+
+    var geometry = new THREE.PlaneGeometry(Tetrez.config.dimension.x, Tetrez.config.dimension.y);
+    var material = new THREE.MeshBasicMaterial({ color: 0xffdfba, side: THREE.DoubleSide });
+    var plane = new THREE.Mesh(geometry, material);
+
+    var scene = new THREE.Scene();
+    scene.add(plane);
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(width, height);
+    renderer.setClearColor(0xffffba);
+
+    document.body.appendChild(renderer.domElement);
+
+    var render = function() {
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
     };
 
-    Block.prototype.moveDown = function() {
-        // Increment y coordinates
-        for (var i = 0; i < this.matrix.length; ++i) {
-            ++this.matrix[i][0];
-        };
-    };
+    var isFalling = false;
 
-    // Block.prototype.rotate = function() {
-    //     var pivot = $V(this.matrix[1]);
+    var fallDown = function() {
+        var canMoveToBottom = true;
 
-    //     for (var i = 0; i < block.matrix.length; ++i) {
-    //         $V(block.matrix[i]).rotate(Math.PI / 2, pivot).round();
-    //     }
-    // };
+        // Add a new block if no one is currently falling
+        if (!isFalling) {
+            tetromino = new Tetrez.Tetromino;
+            scene.add(tetromino.mesh);
 
-    var field = [
-        [true, true, true, true],
-        [true, true, true, true],
-        [true, true, true, true],
-        [true, true, true, true]
-    ];
+            isFalling = true;
+        }
 
-    var runs = 2;
+        // Check if move to bottom is possible
+        for (var j = 0; j < tetromino.matrix.length; ++j) {
+            var y = tetromino.matrix[j][0];
+            var x = tetromino.matrix[j][1];
 
-    while (runs) {
-        var block = new Block;
-
-        for (var i = 0; i < field.length; ++i) {
-            var canMoveToBottom = true;
-
-            // Check if move to bottom is possible
-            for (var j = 0; j < block.matrix.length; ++j) {
-                var y = block.matrix[j][0];
-                var x = block.matrix[j][1];
-
-                // Break if next block is occupied or bottom end of field has reached
-                if (!field[y + 1] || !field[y + 1][x]) {
-                    canMoveToBottom = false;
-                    break;
-                }
-            }
-
-            if (canMoveToBottom) {
-                block.moveDown();
-            } else {
-                // Cannot move any further, copy last block position into field
-                for (var j = 0; j < block.matrix.length; ++j) {
-                    var y = block.matrix[j][0];
-                    var x = block.matrix[j][1];
-
-                    field[y][x] = false;
-                }
-
-                console.log(field);
-
+            // Break if next field block is occupied or bottom end of field has reached
+            if (!field[y + 1] || !field[y + 1][x]) {
+                canMoveToBottom = false;
                 break;
             }
         }
 
-        --runs;
-    }
+        if (canMoveToBottom) {
+            tetromino.moveDown();
+        } else {
+            // Cannot move any further, copy last tetromino position into field
+            for (var j = 0; j < tetromino.matrix.length; ++j) {
+                var y = tetromino.matrix[j][0];
+                var x = tetromino.matrix[j][1];
+
+                field[y][x] = 0;
+            }
+
+            isFalling = false;
+
+            // Uncomment for debugging
+            // field.forEach(function(y) {
+            //     console.log(JSON.stringify(y), Math.random());
+            // });
+            // console.log("---------");
+
+            // Quit on game over
+            for (var i = 0; i < field[0].length; ++i) {
+                if (field[0][i] === 0) {
+                    isGameOver = true;
+
+                    break;
+                }
+            }
+
+            if (isGameOver) {
+                console.log("Game Over :-(");
+                clearInterval(gameInterval);
+            }
+        }
+    };
+
+    fallDown();
+    gameInterval = setInterval(fallDown, Tetrez.config.initSpeed);
+
+    render();
+
+    var isKeyRepeating = false;
+
+    window.addEventListener("keydown", function(e) {
+        if (isGameOver) return;
+
+        switch (e.keyCode) {
+            case 37: // Left
+            break;
+
+            case 39: // Right
+            break;
+
+            case 38: // Top
+            break;
+
+            case 40: // Bottom
+                fallDown();
+
+                if (!isKeyRepeating) {
+                    clearInterval(gameInterval);
+                }
+
+                isKeyRepeating = true;
+            break;
+
+            case 32: // Space
+            break;
+        };
+    });
+
+    window.addEventListener("keyup", function(e) {
+        if (isGameOver) return;
+
+        switch (e.keyCode) {
+            case 37: // Left
+            break;
+
+            case 39: // Right
+            break;
+
+            case 38: // Top
+            break;
+
+            case 40: // Bottom
+                gameInterval = setInterval(fallDown, Tetrez.config.initSpeed);
+                isKeyRepeating = false;
+            break;
+
+            case 32: // Space
+            break;
+        };
+    });
+
+    // Uncomment for 3D debugging
+    // window.addEventListener("keydown", function(e) {
+    //     switch (e.keyCode) {
+    //         case 37: // Left
+    //             scene.rotation.y -= (Math.PI / 30);
+    //         break;
+
+    //         case 39: // Right
+    //             scene.rotation.y += (Math.PI / 30);
+    //         break;
+    //     };
+    // });
 }());
-
-// (function() {
-//     var width = window.innerWidth;
-//     var height = window.innerHeight;
-//     var aspectRatio = width / height;
-//     var viewSize = 20;
-
-//     var field = {
-//         x: 10,
-//         y: 18
-//     };
-
-//     var camera = new THREE.OrthographicCamera(
-//         -(aspectRatio * viewSize / 2),
-//         (aspectRatio * viewSize / 2),
-//         (viewSize / 2),
-//         -(viewSize / 2)
-//     );
-
-//     camera.position.z = viewSize;
-
-//     var blocks = {
-//         t: (function() {
-//             var shape = new THREE.Shape();
-//             shape.moveTo(0, 0);
-//             shape.lineTo(0, 1);
-//             shape.lineTo(1, 1);
-//             shape.lineTo(1, 2);
-//             shape.lineTo(2, 2);
-//             shape.lineTo(2, 1);
-//             shape.lineTo(3, 1);
-//             shape.lineTo(3, 0);
-
-//             var geometry = new THREE.ExtrudeGeometry(shape, {
-//                 amount: 1,
-//                 bevelSize: 0,
-//                 bevelSegments: 0
-//             });
-
-//             // Adjust center point (for correct rotations)
-//             geometry.translate(-1.5, -.5, -.5);
-
-//             var material = new THREE.MeshBasicMaterial({
-//                 color: 0xffb3ba
-//             });
-
-//             return new THREE.Mesh(geometry, material);
-//         }())
-//     };
-
-//     var geometry = new THREE.PlaneGeometry(field.x, field.y);
-//     var material = new THREE.MeshBasicMaterial({ color: 0xffdfba, side: THREE.DoubleSide });
-//     var plane = new THREE.Mesh(geometry, material);
-
-//     var scene = new THREE.Scene();
-//     scene.add(plane);
-//     scene.add(blocks.t);
-
-//     var renderer = new THREE.WebGLRenderer();
-//     renderer.setSize(width, height);
-//     renderer.setClearColor(0xffffba);
-
-//     var render = function() {
-//         requestAnimationFrame(render);
-//         renderer.render(scene, camera);
-//     };
-
-//     // Place cube at top
-//     blocks.t.position.y = 7.5;
-//     blocks.t.position.x -= .5;
-
-//     document.body.appendChild(renderer.domElement);
-
-//     var moveInterval = setInterval(function() {
-//         if (blocks.t.position.y === -8.5) {
-//             return clearInterval(moveInterval);
-//         }
-
-//         if (canMoveBottom()) {
-//             blocks.t.position.y -= 1;
-//         }
-//     }, 500);
-
-//     var canMoveBottom = function() {
-//         return true;
-//     };
-
-//     window.addEventListener("keydown", function(e) {
-//         switch (e.keyCode) {
-//             case 37: // left
-//                 blocks.t.position.x -= 1;
-//             break;
-
-//             case 39: // right
-//                 blocks.t.position.x += 1;
-//             break;
-
-//             case 38: // top
-//                 blocks.t.rotation.z += Math.PI / 2;
-//             break;
-
-//             case 40: // bottom
-//                 blocks.t.rotation.z -= Math.PI / 2;
-//             break;
-
-//             case 32: // space
-//                 if (canMoveBottom()) {
-//                     blocks.t.position.y = -8.5;
-//                 }
-//             break;
-//         };
-//     });
-
-//     render();
-// }());
