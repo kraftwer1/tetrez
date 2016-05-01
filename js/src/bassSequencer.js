0bcc7148e1025106e8770b03de94e54a97189917
Alex Kühne <alex@nordformat.ch>
2016-05-01 15:45:24 +0200

    Extended bassline

--- js/src/bassSequencer.js
+++ js/src/bassSequencer.js
@@ -15,113 +15,64 @@
                 if (isMuted) return;
 
                 switch (bassSequencerStep) {
-                    case 1:
-                    break;
-
                     case 2:
                         createjs.Sound.play("bass2");
-                    break;
-
-                    case 3:
                     break;
 
                     case 4:
                         createjs.Sound.play("bass3");
                     break;
 
-                    case 5:
-                    break;
-
                     case 6:
                         createjs.Sound.play("bass3");
-                    break;
-
-                    case 7:
-                    break;
-
-                    case 8:
-                    break;
-
-                    case 9:
-                    break;
-
-                    case 10:
-                    break;
-
-                    case 11:
-                    break;
-
-                    case 12:
-                    break;
-
-                    case 13:
-                    break;
-
-                    case 14:
-                    break;
-
-                    case 15:
-                    break;
-
-                    case 16:
-                    break;
-
-                    case 17:
-                    break;
-
-                    case 18:
-                    break;
-
-                    case 19:
                     break;
 
                     case 20:
                         createjs.Sound.play("bass3");
                     break;
 
-                    case 21:
-                    break;
-
                     case 22:
                         createjs.Sound.play("bass3");
                     break;
 
-                    case 23:
+                    case 34:
+                        createjs.Sound.play("bass2");
                     break;
 
-                    case 24:
+                    case 36:
+                        createjs.Sound.play("bass3");
                     break;
 
-                    case 25:
+                    case 38:
+                        createjs.Sound.play("bass3");
                     break;
 
-                    case 26:
+                    case 52:
+                        createjs.Sound.play("bass3");
+                    break;
+
+                    case 54:
+                        createjs.Sound.play("bass3");
+                    break;
+
+                    case 58:
                         createjs.Sound.play("bass1");
                     break;
 
-                    case 27:
+                    case 59:
                         createjs.Sound.play("bass4");
                     break;
 
-                    case 28:
-                    break;
-
-                    case 29:
+                    case 61:
                         createjs.Sound.play("bass4");
                     break;
 
-                    case 30:
-                    break;
-
-                    case 31:
-                    break;
-
-                    case 32:
+                    case 64:
                         createjs.Sound.play("bass1");
                     break;
                 }
 
-                if (bassSequencerStep === 32) {
+                if (bassSequencerStep === 64) {
                     bassSequencerStep = 1;
                 } else {
                     ++bassSequencerStep;
